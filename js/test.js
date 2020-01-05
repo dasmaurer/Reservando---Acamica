3ab68c4479c56adee81a233d9ac9719b67f4b6e1
Maurer <dasmaurer@gmail.com>
2020-01-04 23:28:17 -0300

    Planteo de test.js OK, test.html correctamente linkeado

---
+++ js/test.js
@@ -0,0 +1,51 @@
+var assert = chai.assert;
+var expect = chai.expect;
+
+
+// Paso 2: Testeá la función reservarHorario(horario) ///////////////////////////////////////////////////////////////////////////////////
+
+describe("testear funcionalidades de reservarHorario()", function() {
+    //  Cuando se reserva un horario de un restaurant, el horario correspondiente se elimina del arreglo.
+    it("Debería eliminar el horario reservad odel arreglo", function(){
+    });
+    // Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.
+    it("Debería no modificar el array de horarios si se reserva un horario no valido", function(){
+
+    });
+    // Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual
+    it("El arreglo debería no ser modificado cuando no se le pasa parámetro a la Fn()", function(){
+
+    });
+});
+
+
+// Paso 3: Testeá la función obtenerPuntuación()  ///////////////////////////////////////////////////////////////////////////////////////////
+
+describe("testear funcionalidades de obtenerPuntuacion()", function() {
+
+    // Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.
+    it("Debería calcular correctamente el promedio", function() {
+
+    });
+
+
+    // Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.
+    it("Debería mostrar puntuación 0 para restaurantes no calificados", function(){
+
+    })
+});
+
+
+// Paso 4: Testeá la función calificar()  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+describe("testear funcionalidades de calificar()", function(){
+
+
+});
+
+// Paso 5: Testeá la función buscarRestaurante(id)  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// Paso 6: Testeá la función obtenerRestaurantes()  ////////////////////////////////////////////////////////////////////////////////////////////////////
