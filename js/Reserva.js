694c0d3593cca50139c79b64d4551e3b697b8dc3
Maurer <dasmaurer@gmail.com>
2020-01-12 14:35:39 -0300

    Objeto Reserva creado

---
+++ js/Reserva.js
@@ -0,0 +1,16 @@
+var Reserva = function (id, horario, cantPersonas, precioXPersona, codigoDesc) {
+    this.id = id;
+    this.horario = Date;
+    this.cantPersonas = personas;
+    this.precioXPersona = precioXPersona;
+    this.codigoDesc = "";
+};
+
+
+Reserva.prototype.calcularPrecioBase = function() {
+
+};
+
+Reserva.prototype.calcularPrecioTotal = function() {
+
+};
